c80cea046ef1107bc999cb2fc1eac6e8f10b301e
Muhammad Zainur <muhammadzainal2194@gmail.com>
2024-10-01 09:00:45 +0700

    add features

--- index.js
+++ index.js
@@ -1,39 +1,65 @@
 import inquirer from 'inquirer';
-const answers = await inquirer.prompt([
-    {
-        type: 'number',
-        name: 'numberOne',
-        message: 'Kindly enter your first no: ',
-    },
-    {
-        type: 'number',
-        name: 'numberTwo',
-        message: 'Kindly enter your second no: ',
-    },
-    {
-        type: 'list',
-        name: 'operator',
-        choices: ['+', '-', '*', '/'],
-        message: 'Selct operator: ',
-    },
-]);
-const { numberOne, numberTwo, operator } = answers;
-if (numberOne && numberTwo && operator) {
-    let result = 0;
-    if (operator === '+') {
-        result = numberOne + numberTwo;
+let continueCalculation = true; // Flag to control the loop
+let previousResult = null; // Variable to store the previous result
+while (continueCalculation) {
+    const answers = await inquirer.prompt([
+        {
+            type: 'number',
+            name: 'numberOne',
+            message: previousResult === null
+                ? 'Kindly enter your first number: ' // If no previous result, ask for the first number
+                : `Previous result is ${previousResult}. Enter next number: `, // Otherwise, show previous result
+            when: () => previousResult === null, // Only ask for the first number if no previous result
+        },
+        {
+            type: 'number',
+            name: 'numberTwo',
+            message: 'Kindly enter your second number: ',
+        },
+        {
+            type: 'list',
+            name: 'operator',
+            choices: ['+', '-', '*', '/'],
+            message: 'Select operator: ',
+        },
+    ]);
+    // If there's a previous result, use it as `numberOne`
+    const numberOne = previousResult !== null ? previousResult : answers.numberOne;
+    const numberTwo = answers.numberTwo;
+    const operator = answers.operator;
+    if (numberOne !== undefined && numberTwo !== undefined && operator) {
+        let result = 0;
+        // Perform the calculation based on the operator
+        switch (operator) {
+            case '+':
+                result = numberOne + numberTwo;
+                break;
+            case '-':
+                result = numberOne - numberTwo;
+                break;
+            case '*':
+                result = numberOne * numberTwo;
+                break;
+            case '/':
+                result = numberOne / numberTwo;
+                break;
+        }
+        // Output the result
+        console.log('Result: ', result);
+        // Store the result for the next iteration
+        previousResult = result;
     }
-    else if (operator === '-') {
-        result = numberOne - numberTwo;
+    else {
+        console.log('Invalid input');
     }
-    else if (operator === '*') {
-        result = numberOne * numberTwo;
-    }
-    else if (operator === '/') {
-        result = numberOne / numberTwo;
-    }
-    console.log('Your result is: ', result);
+    // Ask if the user wants to continue
+    const { continueCalc } = await inquirer.prompt({
+        type: 'confirm',
+        name: 'continueCalc',
+        message: 'Do you want to perform another calculation with the previous result?',
+        default: true,
+    });
+    // Update the continue flag
+    continueCalculation = continueCalc;
 }
-else {
-    console.log('Kindly enter valid inputs');
-}
+console.log('Calculation finished.');
--- index.ts
+++ index.ts
@@ -1,43 +1,77 @@
 import inquirer from 'inquirer';
 
-const answers: {
-  numberOne: number;
-  numberTwo: number;
-  operator: string;
-} = await inquirer.prompt([
-  {
-    type: 'number',
-    name: 'numberOne',
-    message: 'Kindly enter your first no: ',
-  },
+let continueCalculation = true; // Flag to control the loop
+let previousResult: number | null = null; // Variable to store the previous result
 
-  {
-    type: 'number',
-    name: 'numberTwo',
-    message: 'Kindly enter your second no: ',
-  },
+while (continueCalculation) {
+  const answers = await inquirer.prompt([
+    {
+      type: 'number',
+      name: 'numberOne',
+      message:
+        previousResult === null
+          ? 'Kindly enter your first number: ' // If no previous result, ask for the first number
+          : `Previous result is ${previousResult}. Enter next number: `, // Otherwise, show previous result
+      when: () => previousResult === null, // Only ask for the first number if no previous result
+    },
+    {
+      type: 'number',
+      name: 'numberTwo',
+      message: 'Kindly enter your second number: ',
+    },
+    {
+      type: 'list',
+      name: 'operator',
+      choices: ['+', '-', '*', '/'],
+      message: 'Select operator: ',
+    },
+  ]);
 
-  {
-    type: 'list',
-    name: 'operator',
-    choices: ['+', '-', '*', '/'],
-    message: 'Selct operator: ',
-  },
-]);
+  // If there's a previous result, use it as `numberOne`
+  const numberOne =
+    previousResult !== null ? previousResult : answers.numberOne;
+  const numberTwo = answers.numberTwo;
+  const operator = answers.operator;
 
-const { numberOne, numberTwo, operator } = answers;
-if (numberOne && numberTwo && operator) {
-  let result: number = 0;
-  if (operator === '+') {
-    result = numberOne + numberTwo;
-  } else if (operator === '-') {
-    result = numberOne - numberTwo;
-  } else if (operator === '*') {
-    result = numberOne * numberTwo;
-  } else if (operator === '/') {
-    result = numberOne / numberTwo;
+  if (numberOne !== undefined && numberTwo !== undefined && operator) {
+    let result: number = 0;
+
+    // Perform the calculation based on the operator
+    switch (operator) {
+      case '+':
+        result = numberOne + numberTwo;
+        break;
+      case '-':
+        result = numberOne - numberTwo;
+        break;
+      case '*':
+        result = numberOne * numberTwo;
+        break;
+      case '/':
+        result = numberOne / numberTwo;
+        break;
+    }
+
+    // Output the result
+    console.log('Result: ', result);
+
+    // Store the result for the next iteration
+    previousResult = result;
+  } else {
+    console.log('Invalid input');
   }
-  console.log('Your result is: ', result);
-} else {
-  console.log('Kindly enter valid inputs');
+
+  // Ask if the user wants to continue
+  const { continueCalc } = await inquirer.prompt({
+    type: 'confirm',
+    name: 'continueCalc',
+    message:
+      'Do you want to perform another calculation with the previous result?',
+    default: true,
+  });
+
+  // Update the continue flag
+  continueCalculation = continueCalc;
 }
+
+console.log('Calculation finished.');
